e1ba76a4a122f60b906af1b28cb92fb732f66cdb
staskosh <staskoshenkov@rambler.ru>
2021-09-30 14:12:12 +0300

    change names of variables

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-read_user_file =  pd.read_excel('wine.xlsx', sheet_name='Лист1', na_values=' ', keep_default_na=False)
-user_dict = read_user_file.to_dict(orient='records')
+user_file =  pd.read_excel('wine.xlsx', sheet_name='Лист1', na_values=' ', keep_default_na=False)
+user_file_dict = user_file.to_dict(orient='records')
 sorted_wines = collections.defaultdict(list)
-for wine in user_dict:
+for wine in user_file_dict:
     category = wine['Категория']
     sorted_wines[category].append(wine)
 sorted_wines = collections.OrderedDict(sorted(sorted_wines.items()))
 actual_date = datetime.datetime.now().year
 foundation_year = 1921
-time_difference = actual_date-foundation_year
+age = actual_date-foundation_year
 
 rendered_page = template.render(
     sorted_wines = sorted_wines,
-    age = time_difference,
+    age = age,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
